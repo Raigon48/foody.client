a8d55a865cd3c22c34c1fccb854cbaffbd056117
Abhishek Rai <raigon48@gmail.com>
2025-09-27 20:42:20 +0530

    add fetchCurrentUser query in userprofileform

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -1,10 +1,20 @@
-import { useUpdateMyUser } from "@/apis/MyUserApi";
+import { useGetMyUser, useUpdateMyUser } from "@/apis/MyUserApi";
 import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
 
 export const UserProfilePage = () => {
+  const { currentUser, isLoading } = useGetMyUser();
   const { updateUser, isPending } = useUpdateMyUser();
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (!currentUser) {
+    return <span>Unable to load user profile</span>;
+  }
   return (
     <UserProfileForm
+      currentUser={currentUser}
       onSave={updateUser}
       isLoading={isPending}
     />
